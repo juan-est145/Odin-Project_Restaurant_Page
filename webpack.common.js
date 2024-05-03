e69e629b7dee7fe9cd8884882a53df7c73312cef
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-05-03 13:34:52 +0200

    Deleted unnecessary space on property name

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports =
 {
-	entry  : './src/index.js',
+	entry : './src/index.js',
 	output:{
 		filename: '[name].bundle.js',
 		path : path.resolve(__dirname, 'dist'),
