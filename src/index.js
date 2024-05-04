f61571330940fa9e04c5b608f9252fed4a126f60
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-05-04 20:36:26 +0200

    Found decent logo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './style.css';
+import prueba from '../imgs/vecteezy_diseno-del-logotipo-de-shish-kebab-png_15724000.png';
 
 //document.querySelector("body").classList.add("body");
+const myImage = new Image();
+myImage.src = prueba;
+
+document.querySelector("body").append(myImage);
