a3cd68f53602a7c6eb9f48afc816530a42f268de
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-05-05 12:53:56 +0200

    Header is done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 import './style.css';
 
-//document.querySelector("body").classList.add("body");
-/*const myImage = new Image();
-myImage.src = prueba;
-
-document.querySelector("body").append(myImage);*/
