6ce7a5284db40c2e3c628a862d18fb0fff38f602
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-05-05 18:03:09 +0200

    Home page has been styled accordingly

--- src/main-page.js
+++ src/main-page.js
@@ -1,10 +1,21 @@
+import MainPhoto from '../imgs/pexels-daniel-cruz-170431136-13620477.jpg'
+
 export default function renderMainPage()
 {
 	const content = document.querySelector("#content");
 	const title = document.createElement("h1");
 	const imageDescription = document.createElement("div");
+	const image = new Image();
+	const textDescription = document.createElement("p");
 
 	title.textContent = "Amigo Kebab is the best Kebab in all of Malaga";
+	imageDescription.classList = "img-description";
+	image.src = MainPhoto;
+	textDescription.textContent = "Our kebabs are made with expertise and love. \
+	Every bite you take makes you feel like a Sultan";
 
 	content.appendChild(title);
+	content.appendChild(imageDescription);
+	imageDescription.appendChild(image);
+	imageDescription.appendChild(textDescription);
 }
