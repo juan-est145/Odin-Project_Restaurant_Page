5e5dc926b91edd0425ecc89949d9432c6a3fbb1e
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-05-05 12:26:15 +0200

    Changed the logo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './style.css';
-import prueba from '../imgs/vecteezy_diseno-del-logotipo-de-shish-kebab-png_15724000.png';
 
 //document.querySelector("body").classList.add("body");
 /*const myImage = new Image();
