a674ed73f3d73b9ad53c3d0391ea14079c2c7a91
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-05-05 18:14:24 +0200

    Added the logic to toggle the correct menu

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
 import './style.css';
 import mainPage from './main-page.js'
 
+const hommeButton = document.querySelector("#home");
+const menuButton = document.querySelector("#menu");
+const aboutButton = document.querySelector("#about");
+const content = document.querySelector("#content");
+
 mainPage();
+
+
+hommeButton.addEventListener("click", () =>
+{
+	while(content.firstChild)
+		content.removeChild(content.firstChild);
+	mainPage();
+});
+
+menuButton.addEventListener("click", () =>
+{
+	while(content.firstChild)
+		content.removeChild(content.firstChild);
+});
+
+aboutButton.addEventListener("click", () =>
+{
+	while(content.firstChild)
+		content.removeChild(content.firstChild);
+});
